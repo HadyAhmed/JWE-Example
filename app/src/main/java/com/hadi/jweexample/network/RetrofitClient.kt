fd75e748ebd60c21a1f7f0987e8951fbbf2b2fd5
Hadi Ahmed <hady.ashour@ibnsina-pharma.com>
2021-04-01 10:53:44 +0200

    - solving issue with body application/json

--- app/src/main/java/com/hadi/jweexample/network/RetrofitClient.kt
+++ app/src/main/java/com/hadi/jweexample/network/RetrofitClient.kt
@@ -3,6 +3,7 @@ package com.hadi.jweexample.network
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -18,7 +19,7 @@ interface ClientApi {
 
 object RetrofitClient {
     fun retrofitClient(keyIdHeaderValue: String? = null): ClientApi = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
         .baseUrl("https://grey.paysky.io:7006")
         .client(HttpClient.provideOkHttpClient(keyIdHeaderValue = keyIdHeaderValue))
         .build()
